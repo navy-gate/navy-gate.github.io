633d7a42fbd41ad7c9354837fc1795bc059728e6
Olivier PINON <pinono@iut-acy.local>
2017-01-12 14:12:26 +0100

    Mon texte de commit

--- dream/main.js
+++ dream/main.js
@@ -7,6 +7,7 @@ var controls;
 var manager = new THREE.LoadingManager();
 var boatgroup = new THREE.Group();
 var bateauActuel;
+var propulsionActuelle;
 var offsetBoat;
 
 manager.onStart = function()
@@ -38,8 +39,14 @@ function loadassets()
 	loader = new THREE.JSONLoader(manager);
 
 	loadModel("BateauA.json", 1);
+	loadModel("BateauB.json", 2);
 	loadModel("BateauC.json", 5);
 	loadModel("Sea.json", 1);
+	loadModel("Rames.json", 0.7);
+	loadModel("BoatProp12.json", 1);
+	loadModel("BoatProp16.json", 1);
+	loadModel("BoatProp16-PRO.json", 1);
+	loadModel("BoatProp32.json", 1);
 }
 
 function init()
@@ -64,11 +71,12 @@ function init()
 	// LAUNCH THE CONTROLS
 	controls = new THREE.OrbitControls( camera , renderer.domElement);
 
-	// LOADS THE MODEL
+	// LOADS THE DEFAULT BOAT
 	bateauActuel = assets["BateauA.json"];
 	loadPropulsionValues("BateauA");
-
 	offsetBoat = 0.25;
+
+	// LOADS THE SEA MODEL
 	var mer = assets["Sea.json"];
 	scene.add(mer);
 	mer.position.y = -2;
@@ -96,15 +104,23 @@ function init()
 }
 
 var systemePropulsion = {
-	"BateauA": [ "-", "Rames", "V12" ],
-	"BateauB": [ "-", "V16", "V16-PRO" ],
-	"BateauC": [ "-", "V16-PRO", "V32" ]
+	"BateauA": [ "-", "Rames", "BoatProp12" ],
+	"BateauB": [ "-", "BoatProp16", "BoatProp16-PRO" ],
+	"BateauC": [ "-", "BoatProp16-PRO", "BoatProp32" ]
 }
 
 var boatOffsets = {
 	"BateauA":0.25,
-	"BateauB":1,
+	"BateauB":1.5,
 	"BateauC":5,
+}
+
+var offsetPropulsion = {
+	"Rames": { "BateauA": { x:0, y:-0.4, z:0.1}},
+	"BoatProp12": { "BateauA": { x:0.5, y:-1, z:5.5}},
+	"BoatProp16": { "BateauB": { x:0, y:-2, z:7} },
+	"BoatProp16-PRO": { "BateauB": { x:0, y:-2, z:7}, "BateauC": { x:-2, y:-6, z:-20.6}},
+	"BoatProp32": { "BateauC": { x:-3, y:-4.5, z:-21}}
 }
 
 function animate()
@@ -165,6 +181,26 @@ function loadPropulsionValues(nom)
 	}
 }
 
+function loadPropulsionModel(nom)
+{
+	console.log("loading ");
+	if(propulsionActuelle)
+	{
+		boatgroup.remove(propulsionActuelle);
+	}
+
+	if(nom != "-")
+	{
+		propulsionActuelle = assets[nom + ".json"];
+		if(offsetPropulsion[nom] == null)
+		{
+			console.log("test");
+		}
+		pos = offsetPropulsion[nom][$("#modeleBateau").val()];
+		propulsionActuelle.position.set(pos.x, pos.y, pos.z);
+		boatgroup.add(propulsionActuelle);
+	}
+}
 
 $("#modeleBateau").on("change", function(e) {
 
@@ -177,6 +213,12 @@ $("#modeleBateau").on("change", function(e) {
 	// Change the propulsion values
 	loadPropulsionValues(nom);
 
+	// Removes the old propulsion system
+	if(propulsionActuelle)
+	{
+		boatgroup.remove(propulsionActuelle);
+	}
+
 	// Change the boat offset
 	offsetBoat = boatOffsets[nom];
 
@@ -188,4 +230,8 @@ $("#couleurPrimaire").on("change", function(e) {
 
 $("#couleurSecondaire").on("change", function(e) {
 	changeColorBoat(1,$("#couleurSecondaire").val());
+});
+
+$("#propulsion").on("change", function(e) {
+	loadPropulsionModel($("#propulsion").val());
 });
