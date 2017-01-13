0435f43af7aa6f8fa54d9793d4cfad9657d96fc4
Etienne JODRY <jodrye@iut-acy.local>
2017-01-13 11:14:05 +0100

    easter egg

--- scripts/app.js
+++ scripts/app.js
@@ -14,5 +14,55 @@ $(function()
   		$("canvas").css('width', $("#webgl").width());
 	});
   	$("#webgl").bind("wheel mousewheel", function(e) {e.preventDefault()});
+
+  	var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+	n = 0;
+	$(document).keydown(function (e) {
+	    if (e.keyCode === k[n++]) {
+	        if (n === k.length) {
+		        showLightbox();
+	            n = 0;
+	            return false;
+	        }
+	    }
+	    else {
+	        n = 0;
+	    }
+	});
+
+	$('body').keyup(function(event) {
+		if(event.which == 27) {
+			if ($('#easter-egg').length){
+				hideLightbox();
+		    }
+		}
+	});
+	$('body').on("click",'#close', function () {
+		hideLightbox();
+	});
 });
 
+
+function showLightbox() {
+	var easter_egg = $('<div />', {
+		id:"easter-egg"
+	});
+
+	$('<video />', {
+		id:"issu_easter-egg",
+		controls: "controls",
+		src: "easter-egg/rba.mp4"
+	}).appendTo(easter_egg);
+
+	$('<div>',{
+		id:"close",
+		text:"X"
+	}).appendTo(easter_egg);
+
+	$('body').append(easter_egg)
+}
+
+function hideLightbox() {
+	$('#easter-egg').css("overflow","visible");
+	$('div').remove('#easter-egg');
+}
